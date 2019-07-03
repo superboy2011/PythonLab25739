e35c63eb45cebc77aa5da43ffb5a8543941e233c
pouyahmdn <theassassin2013@live.ir>
2019-07-03 09:20:57 +0430

    first working notelyConnection function

--- notely/notelyConnection.py
+++ notely/notelyConnection.py
@@ -1,4 +1,4 @@
-# TODO: login, signup, logout
+# TODO: signup, logout
 # TODO: get note, add note, update note
 # TODO: get folder list, add folder, get folder contents
 # TODO: delete folder, delete note
@@ -10,24 +10,34 @@ session_server = requests.session()
 
 url_server = "http://127.0.0.1:8000/accounts/"
 
+server_res = session_server.get(url_server + "login/")
+if 'csrftoken' in server_res.cookies:
+    csrf_token = server_res.cookies['csrftoken']
+else:
+    csrf_token = 'null'
+
+
+def send_request(url_page, dict_data, type):
+    headers = {'Content-type': 'application/json', "X-CSRFToken": csrf_token, "Referer": (url_server + url_page)}
+    dict_data['csrfmiddlewaretoken'] = csrf_token
+    if type is "POST":
+        return session_server.post(url_server + url_page, data=json.dumps(dict_data), headers=headers)
+    elif type is "GET":
+        return session_server.get(url_server + url_page, data=json.dumps(dict_data), headers=headers)
+    else:
+        print("No type other than GET or POST is supported yet")
+
 
 def login_user(username, password):
-    server_res = session_server.get(url_server + "login/")
-    if 'csrftoken' in server_res.cookies:
-        csrf_token = server_res.cookies['csrftoken']
-    else:
-        csrf_token = 'null'
-    dict_login = {'InUsername': username, 'InPassword': password, 'csrfmiddlewaretoken': csrf_token}
-    res = session_server.post(url_server + "login/", data=json.dumps(dict_login), headers=dict({"Referer": (url_server + "login/")}))
+    res = send_request("login/", {"InUsername": username, "InPassword": password}, "POST")
     if res.status_code == 401:
-        # print(res.json())
-        print("wht")
+        print(res.text)
         return False
     elif res.status_code == 200:
-        # print(res.content)
-        print('noway')
+        print(res.text)
         return True
     else:
         print(res.status_code)
-        # print(res.content)
+        print(res.text)
         return False
+
